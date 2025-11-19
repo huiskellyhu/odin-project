0a1147c3306eb23036db1c0a63e7c63b8f494c23
Kelly H <kellyhu96@gmail.com>
2025-11-18 20:08:38 -0800

    add js problem 11

---
+++ js-exercises/11-tempConversion/tempConversion.js
@@ -0,0 +1,17 @@
+const convertToCelsius = function(temperature) {
+    const formula = (temperature - 32) * 5/9;
+
+    return Math.round(formula * 10) / 10;
+};
+
+const convertToFahrenheit = function(temperature) {
+    const formula = temperature  * 9/5 + 32;
+
+    return Math.round(formula * 10) / 10;
+};
+
+// Do not edit below this line
+module.exports = {
+  convertToCelsius,
+  convertToFahrenheit
+};
