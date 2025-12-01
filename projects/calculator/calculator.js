284de25172388463d7c52bbba02a692fd9f25709
Kelly H <kellyhu96@gmail.com>
2025-12-01 02:28:12 -0800

    add proper error checking for calculations and clear button

--- projects/calculator/calculator.js
+++ projects/calculator/calculator.js
@@ -14,6 +14,19 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(op, a, b){
+    switch (op){
+        case '+':
+            return add(a,b);
+        case '-':
+            return subtract(a,b);
+        case 'x':
+            return multiply(a,b);
+        case '/':
+            return divide(a,b);
+    }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     let firstNum = '';
     let secNum = '';
@@ -43,11 +56,47 @@ document.addEventListener('DOMContentLoaded', () => {
     const operatorSelect = document.getElementById('operators');
     operatorSelect.addEventListener('click', (event) => {
         const op = event.target.textContent.trim();
-        if (!['+', '-', 'x', '/'].includes(op)) return;
+        if (!['+', '-', 'x', '/', '=', 'C/E'].includes(op)) return;
 
+        // if select '=' without firstNum && secNum -- do nothing
+        if (op === '=' && (firstNum === '' || secNum === '')){
+            return;
+        }
+
+        if (op === 'C/E'){
+            firstNum = '';
+            secNum = ''
+            operator = '';
+            choseOperator = false;
+            result.textContent = "Result: ";
+            return;
+        }
+
+        // if no operator previously chosen -- set to true and proceed
+        if(!choseOperator){
+            choseOperator = true;
+        
+        // if firstNum && secNum -- condense first and proceed
+        } else if (secNum != ''){
+            firstNum = operate(operator, Number(firstNum), Number(secNum));
+            secNum = '';
+            console.log('cleared');
+            result.textContent = "Result: " + firstNum;
+    
+        // if secNum == '' -- do nothing
+        } else {
+            return;
+        }
+        
+        // general handling for operator
         operator = op;
-        choseOperator = true;
         console.log("Chose operator:", operator);
-        result.textContent += op;
+        
+        // display new operation if not '=', don't allow condense
+        if(op != '='){
+            result.textContent += op;
+        } else {
+            choseOperator = false;
+        }
     })
 });
